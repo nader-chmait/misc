f264bb9aa7318cf1569195448fe41f19ec2de271
nader-chmait <chmait.nader@gmail.com>
2021-08-22 12:22:23 +1000

    update 1

--- doy_btc_historical.R
+++ doy_btc_historical.R
@@ -1,9 +1,11 @@
-
-h <- readxl::read_excel ("//TennisAustralia.local/files/Users/NChmait/Documents/RDir/HIT/bitcoin_2018-1-1_2021-8-1.xlsx")
+library(dplyr)
+library(janitor)
+h <- readxl::read_excel ("/Users/nchmait/Documents/Rdir/CryptoAnalytics/misc/misc/bitcoin_2018-1-1_2021-8-1.xlsx")
+h <- read.csv("/Users/nchmait/Documents/Rdir/CryptoAnalytics/misc/misc/archive/coin_BinanceCoin.csv", header = T)
 str(h)
 
 h<- h %>% clean_names() 
-h$dt <- as.Date(h$date, "%B-%d-%Y")
+h$dt <- as.Date(h$date, "%B-%d-%Y")#as.Date(h$date, "%B-%d-%Y")
 h$d <- weekdays(h$dt)
 h$w <- lubridate::week(h$dt)
 h$y <- lubridate::year(h$dt)
@@ -11,19 +13,73 @@ h$y <- lubridate::year(h$dt)
 daily <- h %>% select(-date, -dt) 
 
 #AO.presale.matrix <- tidyr::spread(data = AO.presale %>% select(-fname, -priority, -state) , key = source, value = isMail)
-d.open <- daily %>% select(-close,-high,-low,-market_cap,-volume) %>% tidyr::pivot_wider(names_from = d, values_from = open) #values_fill = list(open > 0)
-d.open <- d.open %>% select(y,w,Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) %>%na.omit() %>%  mutate(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"  ), .fns =  ~.*100/Wednesday))
+d.open <- daily %>% select(-close,-high,-low,-market_cap,-volume) %>% tidyr::pivot_wider(names_from = d, values_from = open)  %>%na.omit() #values_fill = list(open > 0)
+d.close <- daily %>% select(-open,-high,-low,-market_cap,-volume) %>% tidyr::pivot_wider(names_from = d, values_from = close)  %>%na.omit() #values_fill = list(open > 0)
+d.high <- daily %>% select(-close,-open,-low,-market_cap,-volume) %>% tidyr::pivot_wider(names_from = d, values_from = high)  %>%na.omit() #values_fill = list(open > 0)
+d.low <- daily %>% select(-close,-high,-open,-market_cap,-volume) %>% tidyr::pivot_wider(names_from = d, values_from = low)  %>%na.omit() #values_fill = list(open > 0)
 
-d.open <- d.open %>% mutate_if(is.numeric,
-                 round,
-                 digits = 1)
+#d.open <- d.open %>% select(y,w,Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) %>%na.omit() %>%  
+#  mutate(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"  ), .fns =  ~.*100/Wednesday))
 
-d.open$dayshigherWed <- rowSums(d.open[,c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"  )]>100) - rowSums(d.open[,c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"  )]<100)
-open <- d.open %>% group_by(dayshigherWed) %>% summarise(n=n())
-
-library(ggplot2)
-open %>% ggplot(aes(x=dayshigherWed, y=n, fill=dayshigherWed<0, label = n)) + 
-  geom_bar(stat = "identity") + geom_label(aes(x=dayshigherWed, y=n))
   
+d.open <- d.open %>% select(y,w,Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) %>%
+  mutate(OpenOverMon = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Monday),
+         OpenOverTue = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Tuesday),
+         OpenOverWed = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Wednesday),
+         OpenOverThu = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Thursday),
+         OpenOverFri = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Friday),
+         OpenOverSat = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Saturday),
+         OpenOverSun = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Sunday)
+         )
+
+d.close <- d.open %>% select(y,w,Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) %>%         
+  mutate(CloseOverMon = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Monday),
+         CloseOverTue = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Tuesday),
+         CloseOverWed = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Wednesday),
+         CloseOverThu = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Thursday),
+         CloseOverFri = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Friday),
+         CloseOverSat = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Saturday),
+         CloseOverSun = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Sunday)
+         )
+
+d.high <- d.open %>% select(y,w,Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) %>%         
+  mutate(HighOverMon = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Monday),
+         HighOverTue = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Tuesday),
+         HighOverWed = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Wednesday),
+         HighOverThu = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Thursday),
+         HighOverFri = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Friday),
+         HighOverSat = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Saturday),
+         HighOverSun = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Sunday)
+         )
+         
+d.low <- d.open %>% select(y,w,Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) %>%
+  mutate(LowOverMon = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Monday),
+         LowOverTue = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Tuesday),
+         LowOverWed = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Wednesday),
+         LowOverThu = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Thursday),
+         LowOverFri = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Friday),
+         LowOverSat = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Saturday),
+         LowOverSun = rowSums(across(c("Monday", "Tuesday",  "Wednesday", "Thursday",  "Friday", "Saturday",  "Sunday"))> Sunday)
+         )
+
+         
+  
+s <- as.data.frame(d.open[,c(1,2,10:16)]) %>% 
+  left_join(as.data.frame(d.close[,c(1,2,10:16)]))  %>% 
+  left_join(as.data.frame(d.high[,c(1,2,10:16)]))  %>% 
+  left_join(as.data.frame(d.low[,c(1,2,10:16)])) 
+
+
+s.tot <- as.data.frame(colSums(s)) %>% slice(-1,-2) %>% rename(count = `colSums(s)`) %>% 
+  add_rownames(var = "metric") 
+
+s.tot$grp <- gsub("Over|Open|Close|High|Low","",s.tot$metric,ignore.case = T)
+s.tot$metric.type <- gsub("Over.*","",s.tot$metric,ignore.case = T)
+ 
+library(ggplot2)
+s.tot %>% ggplot(aes(x=metric, y=count, fill=grp)) + 
+  geom_bar(stat = "identity")  + coord_flip() +
+  theme_minimal() + theme(axis.text.x = element_text(angle = 90, hjust = 1))  + facet_wrap(~ metric.type, scales = "free")
+
             
             
